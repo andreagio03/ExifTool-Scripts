934c374f7eaf69cd4954f0b0500cf65a002a9d98
Andrea <andreagio203@gmail.com>
2025-06-20 20:18:36 +0200

    Update update_dates.sh
    More types of file supported (JPG, HEIC, PNG, MOV)

--- update_dates.sh
+++ update_dates.sh
@@ -2,24 +2,24 @@
 
 echo "Updating EXIF dates for PHOTOS (jpg, jpeg, heic, png)..."
 exiftool -r '-alldates<${filename;s/.*?(\d{8})_(\d{6}).*/$1 $2/}' \
-  -ext jpg -ext jpeg -ext heic -ext png -overwrite_original .
+  -ext jpg -ext JPG -ext jpeg -ext HEIC -ext heic -ext PNG -ext png -overwrite_original .
 
 echo "Updating QuickTime dates for VIDEOS (mp4, mov)..."
 exiftool -r \
   '-QuickTime:CreateDate<${filename;s/.*?(\d{8})_(\d{6}).*/$1 $2/}' \
   '-QuickTime:ModifyDate<${filename;s/.*?(\d{8})_(\d{6}).*/$1 $2/}' \
-  -ext mp4 -ext mov -overwrite_original .
+  -ext mp4 -ext MOV -ext mov -overwrite_original .
 
 echo "Updating FILESYSTEM dates from DateTimeOriginal for PHOTOS..."
 exiftool -r \
   '-FileCreateDate<DateTimeOriginal' \
   '-FileModifyDate<DateTimeOriginal' \
-  -ext jpg -ext jpeg -ext heic -ext png -overwrite_original .
+  -ext jpg -ext JPG -ext jpeg -ext HEIC -ext heic -ext PNG -ext png -overwrite_original .
 
 echo "Updating FILESYSTEM dates from CreateDate for VIDEOS..."
 exiftool -r \
   '-FileCreateDate<CreateDate' \
   '-FileModifyDate<CreateDate' \
-  -ext mp4 -ext mov -overwrite_original .
+  -ext mp4 -ext MOV -ext mov -overwrite_original .
 
 echo "Done!"
