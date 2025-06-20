a30f7f081fe878c59bd57ad33b64540797a49c31
Andrea <andreagio203@gmail.com>
2025-06-20 21:33:39 +0200

    Update update_dates.sh

--- update_dates.sh
+++ update_dates.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-echo "Updating EXIF dates for PHOTOS (jpg, jpeg, heic, png)..."
+echo "Updating EXIF dates for PHOTOS..."
 exiftool -r '-alldates<${filename;s/.*?(\d{8})_(\d{6}).*/$1 $2/}' \
   -ext JPG -ext jpg -ext jpeg -ext HEIC -ext heic -ext PNG -ext png -overwrite_original .
 
-echo "Updating QuickTime dates for VIDEOS (mp4, mov)..."
+echo "Updating QuickTime dates for VIDEOS..."
 exiftool -r \
   '-QuickTime:CreateDate<${filename;s/.*?(\d{8})_(\d{6}).*/$1 $2/}' \
   '-QuickTime:ModifyDate<${filename;s/.*?(\d{8})_(\d{6}).*/$1 $2/}' \
-  -ext mp4 -ext MOV -ext mov -overwrite_original .
+  -ext MP4 -ext mp4 -ext MOV -ext mov -overwrite_original .
 
 echo "Updating FILESYSTEM dates from DateTimeOriginal for PHOTOS..."
 exiftool -r \
@@ -20,6 +20,6 @@ echo "Updating FILESYSTEM dates from CreateDate for VIDEOS..."
 exiftool -r \
   '-FileCreateDate<CreateDate' \
   '-FileModifyDate<CreateDate' \
-  -ext mp4 -ext MOV -ext mov -overwrite_original .
+  -ext MP4 -ext mp4 -ext MOV -ext mov -overwrite_original .
 
 echo "Done!"
