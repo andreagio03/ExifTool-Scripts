ce1bf1db3bbac3fdd9908ad5e91bd6f9f2e56c1c
Andrea <andreagio203@gmail.com>
2025-06-21 09:39:57 +0200

    Update update_dates.sh

--- update_dates.sh
+++ update_dates.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 echo "Updating EXIF dates for PHOTOS..."
-exiftool -r -progress:-'ExifTool %p%% %20b' '-alldates<${filename;s/.*?(\d{8})_(\d{6}).*/$1 $2/}' \
+exiftool -m -r -progress:-'ExifTool %p%% %20b' '-alldates<${filename;s/.*?(\d{8})_(\d{6}).*/$1 $2/}' \
   -ext JPG -ext jpg -ext jpeg -ext HEIC -ext heic -ext PNG -ext png -overwrite_original .
 
 echo "Updating QuickTime dates for VIDEOS..."
-exiftool -r -progress:-'ExifTool %p%% %20b' \
+exiftool -m -r -progress:-'ExifTool %p%% %20b' \
   '-QuickTime:CreateDate<${filename;s/.*?(\d{8})_(\d{6}).*/$1 $2/}' \
   '-QuickTime:ModifyDate<${filename;s/.*?(\d{8})_(\d{6}).*/$1 $2/}' \
   -ext MP4 -ext mp4 -ext MOV -ext mov -overwrite_original .
 
 echo "Updating FILESYSTEM dates from DateTimeOriginal for PHOTOS..."
-exiftool -r -progress:-'ExifTool %p%% %20b' \
+exiftool -m -r -progress:-'ExifTool %p%% %20b' \
   '-FileCreateDate<DateTimeOriginal' \
   '-FileModifyDate<DateTimeOriginal' \
   -ext JPG -ext jpg -ext jpeg -ext HEIC -ext heic -ext PNG -ext png -overwrite_original .
 
 echo "Updating FILESYSTEM dates from CreateDate for VIDEOS..."
-exiftool -r -progress:-'ExifTool %p%% %20b' \
+exiftool -m -r -progress:-'ExifTool %p%% %20b' \
   '-FileCreateDate<CreateDate' \
   '-FileModifyDate<CreateDate' \
   -ext MP4 -ext mp4 -ext MOV -ext mov -overwrite_original .
